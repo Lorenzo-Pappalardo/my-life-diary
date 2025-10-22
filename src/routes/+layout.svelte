fd4d6c39e77a140e3a025872e151df12b715ce44
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2025-10-22 16:33:13 +0200

    Added TailwindCSS and set up global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -7,5 +8,9 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<header>
+	<a href="/" style="text-decoration: none; color: inherit;"><h1>My life</h1></a>
+</header>
 
 {@render children?.()}
