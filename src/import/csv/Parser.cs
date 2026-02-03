ea57cd857f897d28510ad1d615a6973be0dcdf13
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2026-02-03 23:22:41 +0100

    Moved and refactored

--- src/import/csv/Parser.cs
+++ src/import/csv/Parser.cs
@@ -46,18 +46,3 @@ internal partial class Parser
         return errors;
     }
 }
-
-internal class Experience
-{
-    internal required string Title { get; init; }
-    internal required string? Description { get; init; }
-    internal required string Context { get; init; }
-    internal required string Impact { get; init; }
-    internal required string StartDate { get; init; }
-    internal required string? EndDate { get; init; }
-
-    public override string ToString()
-    {
-        return $"{Title} | {Context} | {Impact} | {StartDate} | {EndDate}";
-    }
-}
