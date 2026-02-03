363b89b007776667f8864bb3a4294e68dbfc81e5
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2026-02-03 01:57:09 +0100

    Added parsing of the source CSV file

---
+++ import/Parser.cs
@@ -0,0 +1,62 @@
+using System.Text.RegularExpressions;
+
+internal partial class Parser
+{
+    [GeneratedRegex("^\"?(?<content>.*)\"?(?:,(?<context>[^\",]+),(?<impact>[^\",]+),(?<dates>[^\",]+))$", RegexOptions.IgnoreCase)]
+    private static partial Regex ComplexInput();
+
+    private readonly string path;
+
+    internal Parser(string path)
+    {
+        this.path = path;
+    }
+
+    internal async Task<List<string>> Parse()
+    {
+        List<string> errors = [];
+
+        foreach (string line in await File.ReadAllLinesAsync(path))
+        {
+            var matches = ComplexInput().Match(line);
+
+            if (matches.Success)
+            {
+                var content = matches.Groups["content"].Value;
+                var dates = matches.Groups["dates"].Value;
+                var splits = dates.Split(" - ");
+
+                var e = new Experience()
+                {
+                    Title = content[..Math.Min(100, content.Length)],
+                    Description = content,
+                    Context = matches.Groups["context"].Value,
+                    Impact = matches.Groups["impact"].Value,
+                    StartDate = splits[0],
+                    EndDate = splits.Length > 1 ? splits[1] : null
+                };
+
+                Console.WriteLine(e);
+            }
+            else
+                errors.Add(line);
+        }
+
+        return errors;
+    }
+}
+
+internal class Experience
+{
+    internal required string Title { get; init; }
+    internal required string? Description { get; init; }
+    internal required string Context { get; init; }
+    internal required string Impact { get; init; }
+    internal required string StartDate { get; init; }
+    internal required string? EndDate { get; init; }
+
+    public override string ToString()
+    {
+        return $"{Title} | {Context} | {Impact} | {StartDate} | {EndDate}";
+    }
+}
---
+++ import/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    Console.WriteLine("Path to files to import:");
+    string? path = args[0] ?? Console.ReadLine();
+
+    if (!File.Exists(path))
+    {
+        Console.WriteLine("Invalid path.");
+        return 1;
+    }
+
+    Parser p = new(path);
+    List<string> errors = await p.Parse();
+
+    foreach (string error in errors)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Error.WriteLine(error);
+    }
+
+    return errors.Count > 0 ? 1 : 0;
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    return 1;
+}
