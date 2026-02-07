904630f0f842594bdf40beb168dde0889e734bee
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2026-02-07 01:55:23 +0100

    Fixed generating files when special characters are present in the original title

--- src/routes/export/worker.ts
+++ src/routes/export/worker.ts
@@ -1,27 +1,36 @@
 import { writeFileSync } from 'node:fs';
-import { workerData } from 'node:worker_threads';
+import { parentPort, workerData } from 'node:worker_threads';
 
-const { outputDirectoryName, event } = workerData as {
-	outputDirectoryName: string;
-	event: {
-		title: string;
-		description: string | null;
-		context: string;
-		startDate: Date;
-		endDate: Date | null;
-		impact: boolean;
+const main = () => {
+	const { outputDirectoryName, event } = workerData as {
+		outputDirectoryName: string;
+		event: {
+			title: string;
+			description: string | null;
+			context: string;
+			startDate: Date;
+			endDate: Date | null;
+			impact: boolean;
+		};
 	};
+
+	let markdown = '';
+	markdown += `# ${event.title}\n\n`;
+	markdown += `Context: ${event.context}\n`;
+	markdown += `Period: ${dateToString(event.startDate)}${event.endDate !== null ? '-' + dateToString(event.endDate) : ''}\n`;
+	markdown += `Impact: ${event.impact}\n\n`;
+	markdown += event.description;
+
+	writeFileSync(`${outputDirectoryName}/${sanitiseTitle(event.title)}.md`, markdown);
+	parentPort?.postMessage(true);
 };
 
 const dateToString = (date: Date): string => {
 	return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 };
 
-let markdown = '';
-markdown += `# ${event.title}\n\n`;
-markdown += `Context: ${event.context}\n`;
-markdown += `Period: ${dateToString(event.startDate)}${event.endDate !== null ? '-' + dateToString(event.endDate) : ''}\n`;
-markdown += `Impact: ${event.impact}\n\n`;
-markdown += event.description;
+const sanitiseTitle = (title: string): string => {
+	return title.replace(/<|>|:|"|\/|\\|\||\?|\*/g, '@');
+};
 
-writeFileSync(`${outputDirectoryName}/${event.title}.md`, markdown);
+main();
