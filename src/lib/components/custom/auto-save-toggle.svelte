1217bdf1e6db1ef73f9b9152245e669614cc1af6
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2026-02-15 02:13:03 +0100

    Implemented a simple auto-save button

---
+++ src/lib/components/custom/auto-save-toggle.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { Toggle } from '$lib/components/ui/toggle/index.js';
+	import { onDestroy } from 'svelte';
+
+	let { defaultValue = $bindable<boolean>(true), callback = $bindable<() => unknown>() } = $props();
+
+	const getPressed = () => {
+		return autoSavingIntervalID !== undefined;
+	};
+
+	const setPressed = () => {
+		autoSavingIntervalID = handleAutoSave();
+	};
+
+	const handleAutoSave: () => undefined | number = () => {
+		if (autoSavingIntervalID !== undefined) {
+			return disableAutoSave();
+		}
+
+		return enableAutoSave();
+	};
+
+	const enableAutoSave: () => number = () => {
+		const id = window.setInterval(() => {
+			callback();
+		}, 30000);
+
+		console.log(`Auto-save enabled (#${id})`);
+		return id;
+	};
+
+	const disableAutoSave: () => undefined = () => {
+		console.log('Auto-save disabled');
+		window.clearInterval(autoSavingIntervalID);
+		return (autoSavingIntervalID = undefined);
+	};
+
+	let autoSavingIntervalID: undefined | number = $state(undefined);
+
+	if (defaultValue) {
+		autoSavingIntervalID = handleAutoSave();
+	}
+
+	onDestroy(() => {
+		if (autoSavingIntervalID !== undefined) {
+			disableAutoSave();
+		}
+	});
+</script>
+
+<Toggle bind:pressed={getPressed, setPressed}>Auto save</Toggle>
