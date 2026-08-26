35d0a011817288f29325dced44a575da70954bb3
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2026-08-26 22:51:08 +0200

    Updated PNPM to v11 and other dependencies and set up volume for postgres

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig, env } from '@prisma/config';
+import { defineConfig, env } from 'prisma/config';
 import { loadEnvFile } from 'node:process';
 
 loadEnvFile();
 
 export default defineConfig({
 	schema: 'prisma/schema.prisma',
-	datasources: {
-		db: {
-			// Provide the connection URL for Migrate and CLI tools via the config.
-			url: env('DATABASE_URL')
-		}
+	datasource: {
+		url: env('DATABASE_URL')
 	}
 });
